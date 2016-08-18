d83e6e21de5247afd09ab065cdbc7881946096b1
Peter Den Hartog <pddenhar@gmail.com>
2016-08-18 16:34:16 -0500

    duck android

--- app/src/main/java/com/fewstreet/coordinatedvideoplayer/ReceiveStartCommandTask.java
+++ app/src/main/java/com/fewstreet/coordinatedvideoplayer/ReceiveStartCommandTask.java
@@ -6,12 +6,13 @@ import android.util.Log;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.nio.ByteBuffer;
 import java.util.Date;
 
 /**
  * Created by peter on 8/17/16.
  */
-public class ReceiveStartCommandTask extends AsyncTask<DatagramSocket,Void,Date> {
+public class ReceiveStartCommandTask extends AsyncTask<DatagramSocket,Void,Long> {
     private final String TAG = "ReceiveStartCommandTask";
     private VideoPlaybackActivity videoPlayer;
 
@@ -21,12 +22,16 @@ public class ReceiveStartCommandTask extends AsyncTask<DatagramSocket,Void,Date>
     }
 
     @Override
-    protected Date doInBackground(DatagramSocket... datagramSockets) {
+    protected Long doInBackground(DatagramSocket... datagramSockets) {
         if(datagramSockets.length == 1){
             byte[] recvBuf = new byte[1500];
             DatagramPacket packet = new DatagramPacket(recvBuf, recvBuf.length);
             try {
                 datagramSockets[0].receive(packet);
+                ByteBuffer buffer = ByteBuffer.allocate(Long.SIZE/Byte.SIZE);
+                buffer.put(packet.getData(), 0, Long.SIZE/Byte.SIZE);
+                buffer.flip();//need flip
+                return buffer.getLong();
             } catch (IOException e) {
                 //e.printStackTrace();
                 Log.d(TAG, "Datagram socket receive was interrupted ");
@@ -36,10 +41,10 @@ public class ReceiveStartCommandTask extends AsyncTask<DatagramSocket,Void,Date>
         return null;
     }
 
-    protected void onPostExecute(Date result) {
+    protected void onPostExecute(Long result) {
         if(result != null) {
             Log.d(TAG, "Got a time to play video: " + result.toString());
-            videoPlayer.setVideoPlaybackTime(123);
+            videoPlayer.setVideoPlaybackTime(result);
         } else {
             Log.d(TAG, "Result from UDP socket was null");
         }
--- app/src/main/java/com/fewstreet/coordinatedvideoplayer/VideoPlaybackActivity.java
+++ app/src/main/java/com/fewstreet/coordinatedvideoplayer/VideoPlaybackActivity.java
@@ -1,11 +1,16 @@
 package com.fewstreet.coordinatedvideoplayer;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 import android.widget.VideoView;
@@ -15,46 +20,9 @@ import java.net.SocketException;
 
 public class VideoPlaybackActivity extends AppCompatActivity {
 
-    private static final int FILE_SELECT_CODE = 0;
     private final String TAG = "VideoPlaybackActivity";
-    private Uri videoURI = null;
     DatagramSocket syncSocket = null;
     ReceiveStartCommandTask syncSocketListener;
-
-    public void chooseVideoClick(View view) {
-        showFileChooser();
-    }
-
-    private void showFileChooser() {
-        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-        intent.setType("*/*");
-        intent.addCategory(Intent.CATEGORY_OPENABLE);
-
-        try {
-            startActivityForResult(
-                    Intent.createChooser(intent, "Select a File to Play"),
-                    FILE_SELECT_CODE);
-        } catch (android.content.ActivityNotFoundException ex) {
-            // Potentially direct the user to the Market with a Dialog
-            Toast.makeText(this, "Please install a File Manager.",
-                    Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        switch (requestCode) {
-            case FILE_SELECT_CODE:
-                if (resultCode == RESULT_OK) {
-                    // Get the Uri of the selected file
-                    Uri uri = data.getData();
-                    Log.d(TAG, "File Uri: " + uri.toString());
-                    videoURI = uri;
-                }
-                break;
-        }
-        super.onActivityResult(requestCode, resultCode, data);
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,10 +36,14 @@ public class VideoPlaybackActivity extends AppCompatActivity {
     }
 
     public void setVideoPlaybackTime(long unixtime) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String videoPath = preferences.getString("filePicker", "");
+        Log.d(TAG, "Path: " + videoPath);
         //for now, just play the video instead of scheduling it
         final VideoView videoView = (VideoView) findViewById(R.id.videoView);
-        if(videoURI != null) {
-            videoView.setVideoURI(videoURI);
+        if(!videoPath.equals("")) {
+            videoView.setVideoPath(videoPath);
+            //videoView.setVideoURI(Uri.parse(videoURI.toString()));
 
             videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener()
             {
@@ -106,4 +78,50 @@ public class VideoPlaybackActivity extends AppCompatActivity {
         syncSocket.close();
     }
 
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+        return true;
+    }
+
+    public void launchSettings(MenuItem item) {
+        Intent i = new Intent(this, SettingsActivity.class);
+        startActivity(i);
+    }
+
+    private static final int FILE_SELECT_CODE = 0;
+    private Uri videoURI = null;
+    public void chooseVideoClick(View view) {
+        showFileChooser();
+    }
+    private void showFileChooser() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("*/*");
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+        try {
+            startActivityForResult(
+                    Intent.createChooser(intent, "Select a File to Play"),
+                    FILE_SELECT_CODE);
+        } catch (android.content.ActivityNotFoundException ex) {
+            // Potentially direct the user to the Market with a Dialog
+            Toast.makeText(this, "Please install a File Manager.",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case FILE_SELECT_CODE:
+                if (resultCode == RESULT_OK) {
+                    // Get the Uri of the selected file
+                    Uri uri = data.getData();
+                    Log.d(TAG, "File Uri: " + uri.toString());
+                    videoURI = uri;
+                }
+                break;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 }
